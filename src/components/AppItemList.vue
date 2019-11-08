c68cb8800666ac78447e487ccae3106d787e8b3d
Carine <carine_neris@hotmail.com>
2019-11-07 23:54:16 -0300

    Aula 04- componentes

--- src/components/AppItemList.vue
+++ src/components/AppItemList.vue
@@ -1,39 +1,52 @@
 <template>
-  <div>
-    <h5>Prefixos <span class="badge badge-info"> {{prefixes.length}} </span> </h5>
-            <div class="card">
-              <div class="card-body">
-                <ul class="list-group">
-                  <li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
-										<div class="row">
-											<div class="col-md">
-												{{ prefix }}
-											</div>
-											<div class="col-md text-right">
-												<button class="btn btn-info" v-on:click="deletePrefix(prefix)"><span class="fa fa-trash"></span></button>
-											</div>
-										</div>	
-                  </li>
-                </ul>
-                <br>
-                <div class="input-group">
-                <input class="form-control" type="text" v-model="prefix" v-on:keyup.enter="addPrefix(prefix)" placeholder="Digite o Prefixo"/>
-                <div class="input-group-append">
-                  <button class="btn btn-info" v-on:click="addPrefix(prefix)"><span class="fa fa-plus"></span></button>
-                </div>
-                </div>
+	<div>
+		<h5>{{ title }} <span class="badge badge-info">{{ items.length }}</span></h5>
+		<div class="card">
+			<div class="card-body">
+				<ul class="list-group">
+					<li class="list-group-item" v-for="item in items" v-bind:key="item">
+						<div class="row">
+							<div class="col-md">
+								{{ item }}
 							</div>
-            </div>
-  </div>
+							<div class="col-md text-right">
+								<button class="btn btn-info" v-on:click="deleteItem(item)"><span class="fa fa-trash"></span></button>
+							</div>
+						</div>
+					</li>
+				</ul>
+				<br/>
+				<div class="input-group">
+					<input class="form-control" type="text" v-model="item" v-on:keyup.enter="addItem(item)" placeholder="Digite o item"/>
+					<div class="input-group-append">
+						<button class="btn btn-info" v-on:click="addItem(item)"><span class="fa fa-plus"></span></button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
 	name: "AppItemList",
-	props:["items"]
+	props: ["title", "items"],
+	data() {
+		return {
+			item: ""
+		};
+	},
+	methods: {
+		addItem(item) {
+			this.$emit("addItem", item);
+			this.item = "";
+		},
+		deleteItem(item) {
+			this.$emit("deleteItem", item);
+		}
+	}
 };
-
 </script>
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
 </style>
